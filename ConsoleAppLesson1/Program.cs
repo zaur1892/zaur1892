536a1dfb84bc2e74338c92104bba652844333702
Zaur <zaur.livpool@gmail.com>
2022-10-17 14:16:01 +0400

    Ilkin elave olunma

---
+++ ConsoleAppLesson1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace ConsoleAppLesson1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Digit");
+            int var1;
+
+            while (true)
+            {
+                try
+                {
+                    var1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Siz ancaq integer daxil ede bielrsiniz");
+                }
+
+            }
+
+
+
+
+
+            Console.WriteLine("Enter Second Digit");
+            int var2 = int.Parse(Console.ReadLine());
+
+            int sum = var1 + var2;
+            Console.WriteLine("Sum is:" + sum);
+
+        }
+        public static int Sum(int var1, int var2)
+        {
+            return var1 + var2;
+        }
+
+
+
+    }
+}
+
